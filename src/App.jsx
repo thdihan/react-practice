c110eeba1f69d7a4036617d59147d496968820b1
thdihan <thdihan@outlook.com>
2023-07-25 22:01:09 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+// import './assets/css/dark.css';
+import GlobalStyle from "./components/styles/Global.styles";
+import Tutorial from "./components/Tutorial";
+
 export default function App() {
-    return <div className="app">Hello World</div>;
+    return (
+        <>
+            <GlobalStyle />
+            <Tutorial />
+        </>
+    );
 }
