209748a27abbdd8d6ad5b3d1b69862a9ecfa17f9
thdihan <thdihan@outlook.com>
2023-07-25 20:24:58 +0600

    branch complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import "./assets/css/global.css";
+import Logo from "./components/Logo";
+
 export default function App() {
-    return <div className="app">Hello World</div>;
+    return (
+        <div className="App">
+            <h1>Learn with Sumit</h1>
+            <Logo />
+        </div>
+    );
 }
